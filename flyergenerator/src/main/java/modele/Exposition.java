c227bb49b8a343ef4759e20471be20cfc08a1c7f
SERVAGENT <anthony.servagent@etu.univ-lyon1.fr>
2020-10-27 23:19:09 +0100

    ajout Exposition

---
+++ flyergenerator/src/main/java/modele/Exposition.java
@@ -0,0 +1,20 @@
+package modele;
+
+/**.
+ * Classe de l'evènement du type Exposition.
+ * Elle herite de Event.
+ */
+public class Exposition extends Event {
+    /**.
+     * id de l'exposition
+     */
+    private int idExposition;
+    /**.
+     * id de l'event general
+     */
+    private int idEvent;
+    /**.
+     * description du theme de l'exposition
+     */
+    private String theme;
+}
