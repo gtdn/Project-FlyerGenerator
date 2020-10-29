4c29dc48410aae2cf52808d5a3ca00a1ffae3d29
REJAUMONT <quentin.rejaumont@etu.univ-lyon1.fr>
2020-10-29 18:12:29 +0100

    Instanciation UserDAO

--- flyergenerator/src/main/java/servlets/Login.java
+++ flyergenerator/src/main/java/servlets/Login.java
@@ -2,7 +2,7 @@ package servlets;
 
 import classes.User;
 import dao.UserDAO;
-import javax.servlet.ServletConfig;   
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import javax.persistence.EntityManager;
-
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 /**.
  * Pour la demande connection
@@ -24,25 +24,46 @@ public class Login extends HttpServlet {
      * traiter pour inscription
      * @param request requete
      * @param response react
+     * @param servletconfig servletconfig
      * @throws ServletException si il a erreur
      * @throws IOException pour erreur
      */
-    
+
+     /**.
+     * Entity manager pour faire le lien avec la base de données
+    */
     private EntityManager em;
+    /**.
+     * DAO
+    */
     private UserDAO userdao;
 
-    
-    public void init(ServletConfig servletconfig) 
-    throws ServletException 
-{ 
+    /**.
+     * Instancies les DAO
+     * @param servletconfig paramètre
+     * @throws ServletException erreurs
+    */
+
+    public final void init(final ServletConfig servletconfig)
+    throws ServletException {
     super.init(servletconfig);
-    this.em = Persistence.createEntityManagerFactory("flyergenerator").createEntityManager();
+    final EntityManagerFactory factory;
+    factory = Persistence.createEntityManagerFactory("flyergenerator");
+    em =  factory.createEntityManager();
     this.userdao = new UserDAO(em);
-    
-     
 }
+
+    /**.
+     * Methode post
+     * @param request requete
+     * @param response reponse
+     * @param servletconfig servlet config
+     * @throws ServletException erreurs
+     * @throws IOException autre erreurs
+    */
     protected final void doPost(final HttpServletRequest request,
-                         final HttpServletResponse response, ServletConfig servletconfig)
+                         final HttpServletResponse response,
+                        final ServletConfig servletconfig)
             throws ServletException, IOException {
         init(servletconfig);
         String pseudo = request.getParameter("pseudo");
